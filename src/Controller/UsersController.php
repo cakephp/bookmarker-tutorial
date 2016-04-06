0e0e5782c290c926e9a06585c229a5d647cbca25
Mark Story <mark@mark-story.com>
2016-04-05 22:34:10 -0400

    Whitelist users/logout.

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -12,7 +12,7 @@ class UsersController extends AppController
 {
     public function beforeFilter(\Cake\Event\Event $event)
     {
-        $this->Auth->allow(['add']);
+        $this->Auth->allow(['add', 'logout']);
     }
 
     /**
